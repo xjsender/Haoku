fa22794d5a4f39a63d76a43214a1d0935329e23d
Hao Liu <mouse.mliu@gmail.com>
2015-03-25 21:41:39 +0800

    update redirect_uri

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 var config = {
-    // "redirect_uri": "https://node-jstree.herokuapp.com/oauth/callback"
-    "redirect_uri": "http://localhost:3000/oauth/callback"
+    "redirect_uri": "https://node-jstree.herokuapp.com/oauth/callback"
+    // "redirect_uri": "http://localhost:3000/oauth/callback"
 };
 
 module.exports = config;
